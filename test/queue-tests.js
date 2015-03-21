8b61bc0fa6f16685a8f496f958122d6bd7923ea3
vz <vladizhidkov@gmail.com>
2015-03-21 11:14:19 -0600

    queue

--- test/queue-tests.js
+++ test/queue-tests.js
@@ -7,15 +7,33 @@ var Queue = require('./../queue');
 describe('Queue', function () {
     var queue;
     beforeEach(function () {
+        console.log('beforeEach1');
         queue = new Queue();
     });
 
     describe('enqueue', function () {
-        it('should contain one element', function () {
+        it('should contain 1', function () {
             queue.enqueue('1');
             assert.equal('1', queue.toString());
+        });
+        it('should contain 1,2', function () {
+            queue.enqueue('1');
             queue.enqueue('2');
             assert.equal('1,2', queue.toString());
+        });
+    });
+
+    describe('dequeue', function () {
+
+        //queue.enqueue('1');
+        //queue.enqueue('2');
+
+        it('should return 1', function () {
+            assert.equal('1', queue.dequeue());
+        });
+
+        it('should return 2', function () {
+            assert.equal('2', queue.dequeue());
         });
     });
 });
