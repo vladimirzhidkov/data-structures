761e866c8ce08f7577a8226a4503df1b4e663f4e
vz <vladizhidkov@gmail.com>
2015-03-21 11:55:15 -0600

    queue test

--- test/queue-tests.js
+++ test/queue-tests.js
@@ -7,11 +7,10 @@ var Queue = require('./../queue');
 describe('Queue', function () {
     var queue;
     beforeEach(function () {
-        console.log('beforeEach1');
         queue = new Queue();
     });
 
-    describe('enqueue', function () {
+    describe('enqueue()', function () {
         it('should contain 1', function () {
             queue.enqueue('1');
             assert.equal('1', queue.toString());
@@ -23,17 +22,64 @@ describe('Queue', function () {
         });
     });
 
-    describe('dequeue', function () {
+    describe('dequeue()', function () {
 
-        //queue.enqueue('1');
-        //queue.enqueue('2');
+        beforeEach(function () {
+            queue.enqueue('1');
+            queue.enqueue('2');
+        });
 
         it('should return 1', function () {
             assert.equal('1', queue.dequeue());
         });
 
         it('should return 2', function () {
+            queue.dequeue();
             assert.equal('2', queue.dequeue());
+        });
+
+        it('should return undefined', function () {
+            queue.dequeue();
+            queue.dequeue();
+            assert.equal(undefined, queue.dequeue());
+        });
+    });
+
+    describe('clear()', function () {
+
+        it('queue should be empty', function () {
+            queue.enqueue('1');
+            queue.enqueue('2');
+            queue.clear();
+            assert.equal(undefined, queue.dequeue());
+            assert.equal(0, queue.size());
+            assert.equal(true, queue.isEmpty());
+        });
+    });
+
+    describe('size()', function () {
+
+        it('should return 0', function () {
+            assert.equal(0, queue.size());
+        });
+
+        it('should return 2', function () {
+            queue.enqueue('1');
+            queue.enqueue('2');
+            assert.equal(2, queue.size());
+        });
+    });
+
+    describe('isEmpty()', function () {
+
+        it('should return true', function () {
+            assert.equal(true, queue.isEmpty());
+        });
+
+        it('should return false', function () {
+            queue.enqueue('1');
+            queue.enqueue('2');
+            assert.equal(false, queue.isEmpty());
         });
     });
 });
